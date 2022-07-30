ba2839b546861e9647b495f215461f51b9dd0a73
Joaco <94091702+kincoder1@users.noreply.github.com>
2022-07-30 19:48:43 -0300

    Update code.js

--- JS/code.js
+++ JS/code.js
@@ -1,19 +1,4 @@
 
-/* weather card */
-/*
-       <div class="card">
-            <div class="card-header">
-                <div class="card-header-city"></div>
-                <div class="card-header-country"></div>
-            </div>
-            <div class="card-body">
-                <p></p>
-            </div>
-        </div>
-*/
-
-
-
 // global variables
 const api_key = '8d1a0c6569b2f521faca2c03782172dd';
 
